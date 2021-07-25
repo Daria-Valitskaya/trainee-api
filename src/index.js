5410643d3300a17ac5022d2dc5a6096883633265
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-07-25 19:21:07 +0300

    пилила код

--- src/index.js
+++ src/index.js
@@ -3,38 +3,46 @@ import markup from './templates/markup.hbs';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '980aef4f6602bffaf56ce8d4b0805479';
 
-// class FilmotekaApiServuse {
-//   constructor() {
-//     this.keyword = '';
-//     this.page = 1;
-//   }
-//   fetchTrendingMovie() {
-//     return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`);
-//   }
-//   fetchMovieByKeyword(keyword) {
-//     return fetch(
-//       `${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.keyword}&page=${this.page}`,
-//     ).then(r => r.json());
-//     console.log(data).then(data => {
-//       this.page += 1;
-//       return data;
-//     });
-//   }
+class FilmotekaApiServise {
+  constructor() {
+    this.filmId = '';
+    this.keyword = '';
+    this.page = 1;
+  }
+  fetchTrendingMovie() {
+    return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`).then(response =>
+      response.json(),
+    );
+  }
+  fetchMovieByKeyword(keyword) {
+    return fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${keyword}`)
+      .then(response => response.json())
+      .then(data => {
+        this.page += 1;
+        return data;
+      });
+  }
+  fetchMovieById(filmId) {
+    return fetch(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}`).then(response => response.json());
+  }
 
-//   fetchMovieById() {
-//     return fetch(`${BASE_URL}movie/496450?api_key=${API_KEY}`);
-//   }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.keyword;
+  }
+  set query(newKeyword) {
+    this.keyword = newKeyword;
+  }
+  get filmId() {
+    return this.filmId;
+  }
+  set filmId(currentId) {
+    this.filmId = currentId;
+  }
+}
 
-//   resetPage() {
-//     this.page = 1;
-//   }
-//   get query() {
-//     return this.keyword;
-//   }
-//   set query(newKeyword) {
-//     this.keyword = newKeyword;
-//   }
-// }
 // https://api.themoviedb.org/3/genre/movie/list?api_key=980aef4f6602bffaf56ce8d4b0805479&page=2
 // добавить page
 
@@ -49,8 +57,8 @@ function fetchMovieByKeyword(keyword) {
     .catch(error => console.log(error));
 }
 console.log(fetchMovieByKeyword('cat'));
-function fetchMovieById(ip) {
-  return fetch(`${BASE_URL}movie/${ip}?api_key=${API_KEY}`)
+function fetchMovieById(filmId) {
+  return fetch(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(query => console.log(query))
     .catch(error => console.log(error));
@@ -65,8 +73,7 @@ function fetchTrendingMovie() {
       const filmCards = markup(films);
       console.log(filmCards);
       refs.cardContainer.innerHTML = filmCards;
-    })
-    .catch(error => console.log(error));
+    });
 }
 
 console.log(fetchTrendingMovie());
