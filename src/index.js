067e5301eaa0bf49393b97a034f1c20d5a9398f4
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-07-25 23:51:20 +0300

    рабочие на классе трендинг муви

--- src/index.js
+++ src/index.js
@@ -5,17 +5,14 @@ const API_KEY = '980aef4f6602bffaf56ce8d4b0805479';
 
 class FilmotekaApiServise {
   constructor() {
-    this.filmId = '';
-    this.keyword = '';
     this.page = 1;
+    this.keyword = '';
+    // this.filmId = '';
   }
   fetchTrendingMovie() {
-    return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}&page=${this.page}`)
-      .then(response => response.json())
-      .then(query => {
-        this.page += 1;
-        return query;
-      });
+    return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`).then(response =>
+      response.json(),
+    );
   }
   fetchMovieByKeyword(keyword) {
     return fetch(
@@ -27,11 +24,11 @@ class FilmotekaApiServise {
         return query;
       });
   }
-  fetchMovieById(filmId) {
-    return fetch(`${BASE_URL}movie/${this.filmId}?api_key=${API_KEY}`).then(response =>
-      response.json(),
-    );
-  }
+  // fetchMovieById(filmId) {
+  //   return fetch(`${BASE_URL}movie/${this.filmId}?api_key=${API_KEY}`).then(response =>
+  //     response.json(),
+  //   );
+  // }
 
   resetPage() {
     this.page = 1;
@@ -43,46 +40,53 @@ class FilmotekaApiServise {
   set query(newKeyword) {
     this.keyword = newKeyword;
   }
-  get filmId() {
-    return this.filmId;
-  }
-  set filmId(currentId) {
-    this.filmId = currentId;
-  }
+  // get filmId() {
+  //   return this.filmId;
+  // }
+  // set filmId(currentId) {
+  //   this.filmId = currentId;
 }
 
 // https://api.themoviedb.org/3/genre/movie/list?api_key=980aef4f6602bffaf56ce8d4b0805479&page=2
 // добавить page
 
-function fetchMovieByKeyword(keyword) {
-  return fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${keyword}`)
-    .then(response => response.json())
-    .then(query => {
-      console.log(query);
-      const filmCards = markup(query);
-      return console.log(filmCards);
-    })
-    .catch(error => console.log(error));
-}
-console.log(fetchMovieByKeyword('cat'));
+// function fetchMovieByKeyword(keyword) {
+//   return fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${keyword}`)
+//     .then(response => response.json())
+//     // .then(films => {
+//     //   const filmCards = markup(films);
+//     //   // console.log(filmCards);
+//     //   refs.cardContainer.innerHTML = filmCards;
+//     });
+// }
+
 function fetchMovieById(filmId) {
-  return fetch(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}`)
-    .then(response => response.json())
-    .then(query => console.log(query))
-    .catch(error => console.log(error));
+  return fetch(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}`).then(response => response.json());
+  // .then(films => {
+  //   const filmCards = markup(films);
+  //   console.log(filmCards);
+  //   refs.cardContainer.innerHTML = filmCards;
+  // });
 }
-
-console.log(fetchMovieById('496450'));
+// console.log(fetchMovieByKeyword('cat'));
+// console.log(fetchMovieById('496450'));
 // добавить page
-function fetchTrendingMovie() {
-  return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`)
-    .then(response => response.json())
-    .then(films => {
-      const filmCards = markup(films);
-      console.log(filmCards);
-      refs.cardContainer.innerHTML = filmCards;
-    });
-}
+// function fetchTrendingMovie() {
+//   return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`)
+//     .then(response => response.json())
+//     .then(films => {
+//       const filmCards = markup(films);
+//       console.log(filmCards);
+//       refs.cardContainer.innerHTML = filmCards;
+//     });
+// }
 
-console.log(fetchTrendingMovie());
+// console.log(fetchTrendingMovie());
 const refs = { cardContainer: document.querySelector('.example') };
+
+const API = new FilmotekaApiServise();
+
+API.fetchMovieByKeyword('keyword').then(films => {
+  const filmCards = markup(films);
+  refs.cardContainer.innerHTML = filmCards;
+});
