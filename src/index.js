c9b3d67dd3aa92bf9f2383219d0f3af36f97eb56
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-07-26 12:06:07 +0300

    рабочий фетч

--- src/index.js
+++ src/index.js
@@ -15,78 +15,87 @@ class FilmotekaApiServise {
     );
   }
   fetchMovieByKeyword(keyword) {
-    return fetch(
-      `${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.keyword}&page=${this.page}`,
-    )
+    return fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${keyword}&page=${this.page}`)
       .then(response => response.json())
-      .then(query => {
+      .then(value => {
         this.page += 1;
-        return query;
+        return value;
       });
   }
-  // fetchMovieById(filmId) {
-  //   return fetch(`${BASE_URL}movie/${this.filmId}?api_key=${API_KEY}`).then(response =>
-  //     response.json(),
-  //   );
-  // }
+  fetchMovieById(filmId) {
+    return fetch(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}`).then(response => response.json());
+  }
 
   resetPage() {
     this.page = 1;
   }
 
-  get query() {
+  get value() {
     return this.keyword;
   }
-  set query(newKeyword) {
+  set value(newKeyword) {
     this.keyword = newKeyword;
   }
-  // get filmId() {
+  // get query() {
   //   return this.filmId;
   // }
-  // set filmId(currentId) {
-  //   this.filmId = currentId;
+  // set query(newId) {
+  //   this.filmId = newId;
+  // }
+  //
 }
-
 // https://api.themoviedb.org/3/genre/movie/list?api_key=980aef4f6602bffaf56ce8d4b0805479&page=2
 // добавить page
 
 // function fetchMovieByKeyword(keyword) {
-//   return fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${keyword}`)
-//     .then(response => response.json())
-//     // .then(films => {
-//     //   const filmCards = markup(films);
-//     //   // console.log(filmCards);
-//     //   refs.cardContainer.innerHTML = filmCards;
-//     });
+//   return fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${keyword}`).then(response =>
+//     response.json(),
+//   );
+//   // .then(films => {
+//   //   const filmCards = markup(films);
+//   //   // console.log(filmCards);
+//   //   refs.cardContainer.innerHTML = filmCards;
 // }
 
-function fetchMovieById(filmId) {
-  return fetch(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}`).then(response => response.json());
-  // .then(films => {
-  //   const filmCards = markup(films);
-  //   console.log(filmCards);
-  //   refs.cardContainer.innerHTML = filmCards;
-  // });
-}
-// console.log(fetchMovieByKeyword('cat'));
+// function fetchMovieById(filmId) {
+//   return fetch(`${BASE_URL}movie/${filmId}?api_key=${API_KEY}`).then(response => response.json());
+//   // .then(films => {
+//   //   const filmCards = markup(films);
+//   //   console.log(filmCards);
+//   //   refs.cardContainer.innerHTML = filmCards;
+//   // });
+// }
+// fetchMovieByKeyword('dog').then(films => {
+//   const filmCards = markup(films);
+//   // console.log(filmCards);
+//   refs.cardContainer.innerHTML = filmCards;
+// });
 // console.log(fetchMovieById('496450'));
 // добавить page
+
 // function fetchTrendingMovie() {
-//   return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`)
-//     .then(response => response.json())
-//     .then(films => {
-//       const filmCards = markup(films);
-//       console.log(filmCards);
-//       refs.cardContainer.innerHTML = filmCards;
-//     });
+//   return fetch(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`).then(response =>
+//     response.json(),
+//   );
 // }
 
-// console.log(fetchTrendingMovie());
+// fetchTrendingMovie().then(films => {
+//   const filmCards = markup(films);
+//   refs.cardContainer.innerHTML = filmCards;
+// });
+// fetchMovieById('496450').then(films => console.log(films));
 const refs = { cardContainer: document.querySelector('.example') };
 
 const API = new FilmotekaApiServise();
-
-API.fetchMovieByKeyword('keyword').then(films => {
-  const filmCards = markup(films);
-  refs.cardContainer.innerHTML = filmCards;
-});
+// по ключевому слову-работает
+// API.fetchMovieByKeyword('dog').then(films => {
+//   const filmCards = markup(films);
+//   refs.cardContainer.innerHTML = filmCards;
+// });
+// популярные фильмы работают
+// API.fetchTrendingMovie().then(films => {
+//   const filmCards = markup(films);
+//   refs.cardContainer.innerHTML = filmCards;
+// });
+// по айдишнику тоже работает
+// API.fetchMovieById('496450').then(films => console.log(films));
